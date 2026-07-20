5a8399a503112a5b6b8be036acb3fd58055f3acb
Mendes88 <mendes.ivo.88@gmail.com>
2026-07-20 20:58:39 +0100

    init: app/src/main/java/pt/criativo/registo/MainActivity.java

---
+++ app/src/main/java/pt/criativo/registo/MainActivity.java
@@ -0,0 +1,77 @@
+package pt.criativo.registo;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView        webView;
+    private FirebaseBridge firebaseBridge;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        esconderBarras();
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        ws.setDomStorageEnabled(true);
+        ws.setAllowFileAccessFromFileURLs(true);
+        ws.setAllowUniversalAccessFromFileURLs(true);
+        ws.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        firebaseBridge = new FirebaseBridge(this, webView);
+        webView.addJavascriptInterface(firebaseBridge, "AndroidFB");
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    private void esconderBarras() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+
+    @Override
+    public void onBackPressed() {}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        esconderBarras();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (firebaseBridge != null) firebaseBridge.destroy();
+        if (webView != null) {
+            webView.removeAllViews();
+            webView.destroy();
+        }
+    }
+}
